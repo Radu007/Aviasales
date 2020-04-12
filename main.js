7b6ef68cb63455af770e051933f7ffb776e0de68
Radu <cazacuraduic@gmail.com>
2020-04-12 14:42:01 +0300

    i've got elements and some functions

---
+++ main.js
@@ -0,0 +1,56 @@
+const formSearch = document.querySelector('.form-search'),
+			inputCitiesFrom = document.querySelector('.input__cities-from'),
+			dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'),
+			inputCitiesTo = document.querySelector('.input__cities-to'),
+			dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
+			inputDateDepart = document.querySelector('.input__date-depart');
+
+
+const city  = ['Chisinau','Moscow','Sofia', 'Buchurest', 'Odesa', 'Timisoara',
+								'Balti', 'Arad', 'Budapesta', 'Criuleni', 'Paris', 'Munchen'];
+
+
+
+const showCity = (input, list) => {
+	list.textContent = '';
+
+	if (input.value !== '') {
+
+		const filterCity = city.filter((item) => {
+			const fixItem = item.toLowerCase();
+			return fixItem.includes(input.value.toLowerCase())
+		})
+		filterCity.forEach((item) => {
+			const li = document.createElement('li');
+			li.classList.add('​dropdown__city');
+			li.textContent = item;
+			list.append(li)
+		})
+	}
+}
+
+inputCitiesFrom.addEventListener('input', () => {
+	showCity(inputCitiesFrom, dropdownCitiesFrom)
+})
+
+inputCitiesTo.addEventListener('input', () => {
+	showCity(inputCitiesTo, dropdownCitiesTo)
+})
+
+dropdownCitiesFrom.addEventListener('click', (e) => {
+	const target = e.target
+	
+	if (target.tagName.toLowerCase() === 'li') {
+		inputCitiesFrom.value = target.textContent;
+		dropdownCitiesFrom.textContent = ''
+	}
+})
+
+dropdownCitiesTo.addEventListener('click', (e) => {
+	const target = e.target
+
+	if (target.tagName.toLowerCase() === 'li') {
+		inputCitiesTo.value = target.textContent;
+		dropdownCitiesTo.textContent = ''
+	}
+})
